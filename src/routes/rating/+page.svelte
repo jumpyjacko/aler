93a75c17d9ec5c36a1d7de5fc0255f4ef034deed
Jackson Ly <bobjacklee27@gmail.com>
2026-06-23 12:37:57 +1000

    add: rating page functionality

--- src/routes/rating/+page.svelte
+++ src/routes/rating/+page.svelte
@@ -1,3 +1,67 @@
-<div>
-this is the "matchmaking" page where the user picks between two pieces of media
-</div>
+<script lang="ts">
+    import { type Series } from "$lib/Series";
+    import { MatchOutcome } from "$lib/rating";
+    import { Glicko } from "$lib/rating/glicko";
+    import { getAllItems } from "$lib/storage/IndexedDB";
+    import { onMount } from "svelte";
+
+    import SeriesCandidate from "./seriesCandidate.svelte";
+
+    let seriesList: Series[] | null = $state(null);
+
+    let candidate1: Series | null = $state(null);
+    let candidate2: Series | null = $state(null);
+
+    const ratingSystem = new Glicko(); // TODO: make this an option that a user could switch with like Elo or Glicko-2
+
+    onMount(async () => {
+        const db: Series[] = await getAllItems("list");
+        if (db.length === 0) return;
+        seriesList = db;
+
+        [candidate1, candidate2] = sampleTwoUnique(seriesList);
+    });
+
+    function handleVote(winner: Series, loser: Series) {
+        ratingSystem.update(winner, loser, MatchOutcome.win);
+        [candidate1, candidate2] = sampleTwoUnique(seriesList!);
+    }
+
+    function handleDraw(winner: Series, loser: Series) {
+        ratingSystem.update(winner, loser, MatchOutcome.draw);
+        [candidate1, candidate2] = sampleTwoUnique(seriesList!);
+    }
+
+    function sampleTwoUnique<T>(list: T[]): [T, T] {
+        if (list.length < 2) {
+            throw new Error(
+                "The list must contain at least 2 items to sample unique values.",
+            );
+        }
+
+        const index1 = Math.floor(Math.random() * list.length);
+        let index2 = Math.floor(Math.random() * list.length);
+        while (index2 === index1) {
+            index2 = Math.floor(Math.random() * list.length);
+        }
+
+        return [list[index1], list[index2]];
+    }
+</script>
+
+{#if candidate1 && candidate2}
+    <div class="flex flex-row justify-around w-full divide-x divide-gray-300">
+        <button
+            onclick={() => handleVote(candidate1!, candidate2!)}
+            class="w-full flex flex-col justify-center items-center"
+        >
+            <SeriesCandidate {...candidate1} />
+        </button>
+        <button
+            onclick={() => handleVote(candidate2!, candidate1!)}
+            class="w-full flex flex-col justify-center items-center"
+        >
+            <SeriesCandidate {...candidate2} />
+        </button>
+    </div>
+{/if}
---
+++ src/routes/rating/seriesCandidate.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let {
+        title,
+        coverImage,
+        status,
+        rating,
+        userStatus,
+        userRating,
+        mmrRating,
+        ratingDeviation,
+    } = $props();
+</script>
+
+<img
+    src={coverImage}
+    alt={title}
+    class="rounded-md shadow-sm h-90 w-60 object-cover"
+/>
+
+<div>{title}</div>
+<div>{userStatus}</div>
+<div>Global: {+rating / 10}</div>
+<div>Yours: {userRating}</div>
+<div>Rating: {mmrRating}</div>
+<div>Rating deviation: {ratingDeviation}</div>
