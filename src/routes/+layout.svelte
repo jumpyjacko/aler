c5b0c72c7d25d09f1a646eba6b96beceb4df2624
Jackson Ly <bobjacklee27@gmail.com>
2026-06-23 13:41:28 +1000

    fix(build): use `resolve()` for routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import { resolve } from '$app/paths';
 
 	let { children } = $props();
 </script>
@@ -8,13 +9,13 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <nav class="sticky left-0 top-0 w-full flex flex-row px-8 py-2 justify-between bg-white">
-    <a href="/">aler</a>
+    <a href="{resolve('/')}">aler</a>
 
     <div class="flex flex-row gap-4">
-        <a href="/">Home</a>
-        <a href="/rating">Rating</a>
-        <a href="/leaderboard">Leaderboard</a>
-        <a href="/statistics">Stats</a>
+        <a href="{resolve('/')}">Home</a>
+        <a href="{resolve('/rating')}">Rating</a>
+        <a href="{resolve('/leaderboard')}">Leaderboard</a>
+        <a href="{resolve('/statistics')}">Stats</a>
     </div>
 </nav>
 
