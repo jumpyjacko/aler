8487983339e81e2d0cd72f9b161334b24d325da1
Jackson Ly <bobjacklee27@gmail.com>
2026-06-23 12:38:06 +1000

    fix: better responsive ui for leaderboard

--- src/routes/leaderboard/seriesListEntry.svelte
+++ src/routes/leaderboard/seriesListEntry.svelte
@@ -4,23 +4,25 @@
 </script>
 
 <div
-    class="grid grid-cols-[3rem_8fr_1fr_1fr_1fr] gap-4 p-4 h-24 w-full items-center border-b border-gray-100"
+    class="grid grid-cols-[3rem_1fr] md:grid-cols-[3rem_8fr_8rem_6rem_6rem] gap-x-4 gap-y-1 p-4 min-h-24 md:h-24 w-full items-center border-b border-gray-100"
 >
     <img
         src={coverImage}
         alt={title}
-        class="rounded-md shadow-sm shrink-0 h-16 w-12 object-cover"
+        class="rounded-md shadow-sm shrink-0 h-16 w-12 object-cover row-span-2 md:row-span-1"
     />
 
-    <div class="truncate sm:wrap-break-word font-medium">{title}</div>
-
-    <div class="text-right text-sm text-gray-600 truncate">{userStatus}</div>
-
-    <div class="text-right text-sm text-gray-600 truncate">
-        Global: {+rating / 10}
+    <div
+        class="md:truncate wrap-break-word font-medium self-end md:self-center"
+    >
+        {title}
     </div>
 
-    <div class="text-right text-sm text-gray-600 truncate">
-        Yours: {userRating}
+    <div
+        class="col-start-2 flex flex-col gap-0.5 md:contents text-left md:text-right text-sm text-gray-600 self-start md:self-center"
+    >
+        <div class="truncate">{userStatus}</div>
+        <div class="truncate">Global: {+rating / 10}</div>
+        <div class="truncate">Yours: {userRating}</div>
     </div>
 </div>
