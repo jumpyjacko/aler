6b1f2960dca00f85358611b39aaf3032ab050c72
Jackson Ly <bobjacklee27@gmail.com>
2026-06-25 12:55:07 +1000

    fix: adjust leaderboard thumbnails

--- src/routes/leaderboard/seriesListEntry.svelte
+++ src/routes/leaderboard/seriesListEntry.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <div
-    class="grid grid-cols-[3rem_1fr] md:grid-cols-[3rem_8fr_8rem_6rem_6rem_8rem] gap-x-4 gap-y-1 p-4 min-h-24 md:h-24 w-full items-center border-b border-text-faded"
+    class="grid grid-cols-[5rem_1fr] md:grid-cols-[3rem_8fr_8rem_6rem_6rem_8rem] gap-x-4 gap-y-1 p-4 min-h-24 md:h-24 w-full items-center border-b border-text-faded"
 >
     <img
         src={coverImage}
         alt={title}
-        class="rounded-xs shadow-sm shrink-0 h-16 w-12 object-cover row-span-2 md:row-span-1"
+        class="rounded-sm md:rounded-xs shadow-sm shrink-0 h-24 w-18 md:h-16 md:w-12 object-cover row-span-2 md:row-span-1"
     />
 
     <div
